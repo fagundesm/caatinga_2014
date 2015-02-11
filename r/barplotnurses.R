0922d4ea6e767332e54e79dda54483890db441be
fagundesm <fagundesm>
2015-02-11 10:18:31 -0300

    adicionado gráfico de RII separadamente para folhas e altura

--- r/barplotnurses.R
+++ r/barplotnurses.R
@@ -1,5 +1,6 @@
 library(reshape2)
 library(ggplot2)
+library(dplyr)
 
 rii<- read.csv ("data/dados_media_facilitacao.csv",h=T)
 rii$bloco <- as.factor(rii$bloco)
@@ -17,3 +18,26 @@ ggplot(media1,aes(y=rii,x=nurse,fill=factor(target)))+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size=15))+
 geom_bar(stat="identity",position="dodge")
 
+
+#Médias nurse/target de folha e altura separadas 
+
+rii<- read.csv ("data/dados_RII.csv",h=T)
+rii$tempo <- as.factor(rii$tempo)
+rii$bloco <- NULL
+str(rii)
+
+#Folhas
+mediafol <- summarise(group_by(rii, nurse, target),rii_medf=mean(folhas_rii))  
+
+ggfol<- ggplot(mediafol,aes(y=rii_medf,x=nurse,fill=factor(target)))+
+  xlab("Nurse") + ylab("RII") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size=15))+
+  geom_bar(stat="identity",position="dodge"); ggfol
+
+#Altura
+mediaalt <- summarise(group_by(rii, nurse, target), rii_meda=mean(altura_rii))
+
+ggalt<- ggplot(mediaalt,aes(y=rii_meda,x=nurse,fill=factor(target)))+
+  xlab("Nurse") + ylab("RII") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size=15))+
+  geom_bar(stat="identity",position="dodge"); ggalt
